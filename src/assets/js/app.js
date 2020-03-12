d7a73df803ca392b29137635424dbb8f7100656f
Monika Waligóra <m1waligora@gmail.com>
2020-03-12 15:50:36 +0100

    add my story section

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -44,3 +44,32 @@ $(".center").slick({
   autoplaySpeed: 1900,
   arrows: false
 });
+
+//Projects section show/hide main-list
+
+const readMoreBtn = document.getElementById("readMore-js");
+const mainList = document.querySelector(".main-list");
+
+readMoreBtn.addEventListener("click", function() {
+  mainList.classList.toggle("visible");
+  mainList.classList.toggle("hidden");
+  $("html,body").animate(
+    {
+      scrollTop: $(".main-list").offset().top
+    },
+    "fast"
+  );
+});
+
+const readLessBtn = document.getElementById("readLess-js");
+
+readLessBtn.addEventListener("click", function() {
+  mainList.classList.toggle("visible");
+  mainList.classList.toggle("hidden");
+  $("html,body").animate(
+    {
+      scrollTop: $(".hangman-game-link").offset().top
+    },
+    "fast"
+  );
+});
